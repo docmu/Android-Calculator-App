c55d9a40ee4a6ba031a4765e546ddbd982f20274
docmu <docm@mymail.vcu.edu>
2019-11-01 12:47:10 -0400

    changed EditText to TextView so users cannot type text into the calculator. The only text available is the buttons.

--- app/src/main/java/com/example/mystackcalculator/MainActivity.java
+++ app/src/main/java/com/example/mystackcalculator/MainActivity.java
@@ -15,8 +15,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText editText;
+    private TextView editText;
     private Button clrBtn, delBtn, equalsBtn;
     private Button zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn;
     private Button openParBtn, closeParBtn;
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
+        editText = (TextView) findViewById(R.id.editText);
 
         clrBtn = (Button) findViewById(R.id.clrBtn);
         delBtn = (Button) findViewById(R.id.delBtn);
@@ -58,44 +60,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void onOperator(View view){
          if (view.getId() == R.id.openParBtn)
-            editText.setText(editText.getText() + " ( ");
+             editText.append(" ( ");
         else if (view.getId() == R.id.closeParBtn)
-            editText.setText(editText.getText() + " ) ");
+             editText.append(" ) ");
         else if (view.getId() == R.id.plusBtn)
-            editText.setText(editText.getText() + " + ");
+             editText.append(" + ");
         else if (view.getId() == R.id.minusBtn)
-            editText.setText(editText.getText() + " - ");
+             editText.append(" - ");
         else if (view.getId() == R.id.multiplyBtn)
-            editText.setText(editText.getText() + " * ");
+             editText.append(" * ");
         else if (view.getId() == R.id.divideBtn)
-            editText.setText(editText.getText() + " / ");
+             editText.append(" / ");
         else if (view.getId() == R.id.powerBtn)
-            editText.setText(editText.getText() + " ^ ");
+             editText.append(" ^ ");
     }
 
     public void onNumber(View view){
         if (view.getId() == R.id.zeroBtn)
-            editText.setText(editText.getText() + "0");
+            editText.append("0");
         else if (view.getId() == R.id.oneBtn)
-            editText.setText(editText.getText() + "1");
+            editText.append("1");
         else if (view.getId() == R.id.twoBtn)
-            editText.setText(editText.getText() + "2");
+            editText.append("2");
         else if (view.getId() == R.id.threeBtn)
-            editText.setText(editText.getText() + "3");
+            editText.append("3");
         else if (view.getId() == R.id.fourBtn)
-            editText.setText(editText.getText() + "4");
+            editText.append("4");
         else if (view.getId() == R.id.fiveBtn)
-            editText.setText(editText.getText() + "5");
+            editText.append("5");
         else if (view.getId() == R.id.sixBtn)
-            editText.setText(editText.getText() + "6");
+            editText.append("6");
         else if (view.getId() == R.id.sevenBtn)
-            editText.setText(editText.getText() + "7");
+            editText.append("7");
         else if (view.getId() == R.id.eightBtn)
-            editText.setText(editText.getText() + "8");
+            editText.append("8");
         else if (view.getId() == R.id.nineBtn)
-            editText.setText(editText.getText() + "9");
+            editText.append("9");
         else if (view.getId() == R.id.pointBtn)
-            editText.setText(editText.getText() + ".");
+            editText.append(".");
+
     }
 
     public void onEquals(View v){
